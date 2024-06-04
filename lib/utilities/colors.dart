b3f365423622181c667bdc66a44d8a4c680744bd
TheBenedict69 <sabenedict09@gmail.com>
2024-06-04 11:06:38 +0530

    v0.0.1 (Color & Font added)

---
+++ lib/utilities/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var c1 = Color(0xffB80C09);
+var c2 = Color(0xff373F51);
+var c3 = Color(0xffE5E7E6);
+var c4 = Color(0xffB7B5B3);
+var c5 = Color(0xff141301);
