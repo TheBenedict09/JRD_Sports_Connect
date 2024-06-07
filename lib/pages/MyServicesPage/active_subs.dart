e0a89a9cce2b45e6cbe76e13df0ce47704f2e831
TheBenedict69 <sabenedict09@gmail.com>
2024-06-07 12:29:57 +0530

    v0.0.10 (My Services - Protocols 2)

--- lib/pages/MyServicesPage/active_subs.dart
+++ lib/pages/MyServicesPage/active_subs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:jrd_s_c/utilities/colors.dart';
 
 class ActiveSubElement extends StatelessWidget {
@@ -8,6 +9,10 @@ class ActiveSubElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String subscriptionName = "Subscription Name";
+    final String timeRange = "01 Jan - 10 Jan";
+    final DateTime now = DateTime.now();
+
     return Padding(
       padding: const EdgeInsets.only(left: 8, right: 8),
       child: Container(
@@ -17,7 +22,97 @@ class ActiveSubElement extends StatelessWidget {
         ),
         child: ListTile(
           onTap: () {
-            
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(
+                    subscriptionName,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  content: Text(
+                    "Validity: $timeRange",
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontSize: 18,
+                        ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        if (now.day < 20) {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                actions: [
+                                  IconButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    icon: Icon(Iconsax.close_circle),
+                                  ),
+                                ],
+                                title: Text(
+                                  "Cannot cancel a Subscription before 20th of a Month!",
+                                  textAlign: TextAlign.center,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.copyWith(fontSize: 20),
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pop(); // Close the second dialog
+                                      Navigator.of(context)
+                                          .pop(); // Close the first dialog
+                                    },
+                                    child: const Text('Yes'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('No'),
+                                  ),
+                                ],
+                                title: Text(
+                                  "Are you sure?",
+                                  textAlign: TextAlign.center,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.copyWith(fontSize: 20),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      },
+                      child: const Text('Stop Service'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                );
+              },
+            );
           },
           title: const Text("Title",
               style: TextStyle(fontWeight: FontWeight.bold)),
